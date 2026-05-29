a3b0ead5f84b9ca807949c12804972ec60a55608
Guss <I202413973@cibertec.edu.pe>
2026-05-28 19:09:49 -0500

    Agrega DTOs para autenticacion JWT

---
+++ src/main/java/com/taskflow/authservice/dto/LoginResponse.java
@@ -0,0 +1,13 @@
+package com.taskflow.authservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class LoginResponse {
+
+    private String token;
+    private String tokenType;
+    private String username;
+}
